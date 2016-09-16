9e0437f2c998bad0fe28674d6308d5d2602a935d
MargaritaSv <margarita.esti@gmail.com>
2016-09-16 13:09:23 +0300

    PeopleBoil.java extends People class

---
+++ PeopleBoil.java
@@ -0,0 +1,16 @@
+package rakiq;
+
+/**
+ * Created by magy on 16.09.16.
+ */
+public class PeopleBoil extends People {
+
+    public PeopleBoil(String name, byte age) {
+        super(name, age);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
