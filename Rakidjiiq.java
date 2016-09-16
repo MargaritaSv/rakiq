4ce71689fcb0244da3238198b29e11e3daf94307
MargaritaSv <margarita.esti@gmail.com>
2016-09-16 13:08:08 +0300

    Rakidjiiq.java extends People class

---
+++ Rakidjiiq.java
@@ -0,0 +1,15 @@
+package rakiq;
+
+/**
+ * Created by magy on 16.09.16.
+ */
+public class Rakidjiiq extends People {
+    public Rakidjiiq(String name, byte age) {
+        super(name, age);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
