7274c3675ba6890a74f0c51426d28f88b64aead1
MargaritaSv <margarita.esti@gmail.com>
2016-09-16 12:58:35 +0300

    method for boil rakiq and remove rakidjiq from tank

--- Rakiq.java
+++ Rakiq.java
@@ -1,10 +1,37 @@
 package rakiq;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Created by magy on 16.09.16.
  */
 public class Rakiq {
+
+    public static final int TIME_TO_BOIL = 200;
+
     public enum RakiqTypes {
         PLUM, APRICOT, FRAPPES;
     }
+
+    private BlockingQueue<Rakidjiiq> tanks = new ArrayBlockingQueue<Rakidjiiq>(1);
+
+    public void boil(Rakidjiiq rakidjiiq) {
+        try {
+            Thread.sleep(TIME_TO_BOIL);
+            tanks.put(rakidjiiq);
+        } catch (InterruptedException ex) {
+            return;
+        }
+    }
+
+    public Rakidjiiq readyWithBoil() {
+        try {
+            return tanks.take();
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
 }
